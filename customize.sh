284648f7a13cabb68406ceb766ae034e354a7639
Lord-Phoenix <lord.phoenix.rus@gmail.com>
2013-12-13 14:07:57 +0300

    Update customize.sh
    minor tweaks

--- customize.sh
+++ customize.sh
@@ -33,15 +33,15 @@ mkdir $logdirname
 
 echo "Tweaking rc.local..."
 cp /etc/rc.local /etc/rc.local.backup
-cp ./rc.local /etc/rc.local
+cp ./rclocal/rc.local /etc/rc.local
 echo "Tweaking rc.local. Done!"
 
-#Adding auto mounting WD ShareSpace at 192.168.11.254
+#Adding auto mounting WD ShareSpace
 
 echo "Adding automount string to fstab..."
 mkdir /mnt/wd
 cp /etc/fstab /etc/fstab.backup
-cat ./fstab.add >> /etc/fstab
+cat ./fstab/fstab.add >> /etc/fstab
 echo "Automount string added!"
 
 #Adding powersaving options to /etc/acpi/power.sh file
