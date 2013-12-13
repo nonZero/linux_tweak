19621bf386c3000c5146697ff284a85c5d3076cb
Lord-Phoenix <lord.phoenix.rus@gmail.com>
2013-12-13 14:31:13 +0300

    Update main_script.sh
    Moved power.sh tweak to a separate script in /tweakers/power.sh

--- main_script.sh
+++ main_script.sh
@@ -51,13 +51,6 @@ done
 #some basic tweak
 
 
-#Adding powersaving options to /etc/acpi/power.sh file
-
-echo "Replacing default power.sh with tweaked power.sh file..."
-cp /etc/acpi/power.sh /etc/acpi/power.sh.backup
-cp ./power.sh /etc/acpi/power.sh
-echo "power.sh tweaked"
-
 #installing all deb files we have in a same folder
 
 echo "Installing all DEB packages in current folder"
