ea010b8b116fcfaa6109c8e34d498882a7a3a2ee
Gourenko Alex <lord.phoenix.rus@gmail.com>
2014-11-26 12:36:23 +0300

    Create start_over.sh

---
+++ wip/start_over.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~
+rm -r ~/Test
+rm -r ~/Google\ Drive/Data/Test
+mkdir ~/Test
+mkdir ~/Google\ Drive/Data/Test
+touch ~/Test/local.file
+touch ~/Google\ Drive/Data/Test/GDrive.file
