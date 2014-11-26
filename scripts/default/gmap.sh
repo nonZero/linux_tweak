186cfa0528fad6c4aef40f651fd0d3feb8029c21
Gourenko Alex <lord.phoenix.rus@gmail.com>
2014-11-26 12:17:25 +0300

    Hopefully final update
    tons of changes, tweaks and changed logic. NOW it should work, damn it

--- scripts/default/gmap.sh
+++ scripts/default/gmap.sh
@@ -3,29 +3,30 @@
 
 function remap {
 
-default_path="Google Drive/Data"
-
-  if [ -n "$1" ]; then
-   default_path = $1
-  fi
-
 #Going to home folder
 cd ~
 
+#Checking whether we should use a different path
+if [ -z "$1" ]; then	
+	default_path="Google Drive/Data"
+	else
+	default_path=$1
+fi
+
 #Mapping Videos folder
-rm -r Videos/ && ln -s ~/"$default_path"/Videos/ Videos
+rm -r ~/Videos/ && ln -s ~/"$default_path"/Videos/ Videos
 echo "Videos folder remaped"
 
 #Mapping Music folder
-rm -r Music/ && ln -s ~/"$default_path"/Music/ Music
+rm -r ~/Music/ && ln -s ~/"$default_path"/Music/ Music
 echo "Music folder remaped"
 
 #Mapping Desktop folder
-rm -r Desktop/ && ln -s ~/"$default_path"/Desktop/ Desktop
+rm -r ~/Desktop/ && ln -s ~/"$default_path"/Desktop/ Desktop
 echo "Desktop folder remaped"
 
 #Mapping Documents folder
-rm -r Documents/ && ln -s ~/"$default_path"/Documents/ Documents
+rm -r ~/Documents/ && ln -s ~/"$default_path"/Documents/ Documents
 echo "Documents folder remaped"
 
 echo "All folders remaped"
@@ -33,37 +34,39 @@ echo "All folders remaped"
 }
 
 function move_data {
-  
-  cd ~
-  
-  $default_path="Google Drive/Data"
-  
-  if [ -n "$1" ]; then
-   default_path = $1
+
+#Going to home folder 
+cd ~
+
+#Checking whether we should use a different path  
+if [ -z "$1" ]; then
+	default_path="Google Drive/Data"
+	else
+	default_path=$1
   fi
   
   echo "Moving Videos folder"
-mv -r Videos "$default_path"/Videos
+mv ~/Videos/* ~/"$default_path"/Videos
   echo "Videos folder moved"
   
   echo "Moving Music folder"
-mv -r Music "$default_path"/Music
+mv ~/Music/* ~/"$default_path"/Music
   echo "Music folder moved"
   
   echo "Moving Desktop folder"
-mv -r Desktop "$default_path"/Desktop
+mv ~/Desktop/* ~/"$default_path"/Desktop
   echo "Desktop folder moved"
   
   echo "Moving Documents folder"
-mv -r Documents "$default_path"/Documents
+mv ~/Documents/* ~/"$default_path"/Documents
   echo "Documents folder moved"
   
   echo "All folders moved"
 }
 
-echo "You're about to remap your local folders (Music, Videos, Desktop, Documents) to your Google Drive"
+echo "You're about to remap your local folders (Documents, Pictures, Videos, Desktop) to your Google Drive\n"
 
-echo "Are these folders currently empty? (y/n)"
+echo "Do you want to move existing data? (y/n)"
 
 read movedata
 
@@ -71,20 +74,25 @@ echo "Do you want to use default path to Google Drive? (~/Google Drive/Data) (y/
 
 read use_default_path
 
-if ["$use_default_path" = "n"]; then
+if [ "$use_default_path" = "n" ]; then
 
- echo "Please type path where Music, Videos, Desktop, Documents folders will be located and press [Enter]"
+ echo "Please type path where Test folder will be located and press [Enter]"
  read actual_path
 
-fi
+else
+	
+	actual_path="Google Drive/Data"
 
+fi	 
 
-if ["$movedata" = "n"]; then
- echo "Moving data now."
-move_data "$actual_path"
- echo "Remaping folders now."
-remap "$actual_path"
+if [ "$movedata" = "y" ]; then
+	
+	echo "Moving data now."
+	move_data "$actual_path"
+	echo "Remaping folders now."
+	remap "$actual_path"
+
  else
-  echo "User data will not be moved. Remapping folders now."
-remap "$actual_path"
+ echo "User data will not be moved. Remapping folders now."
+ remap "$actual_path"
 fi
