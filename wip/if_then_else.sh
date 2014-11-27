fad6b1b08e0c1e2135407deb4e728dab6e6ad71d
Gourenko Alex <lord.phoenix.rus@gmail.com>
2014-11-27 21:44:25 +0400

    Moved WIP file to a different repo

--- wip/if_then_else.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -z "$asdf" ]; then
-	echo There is no asdf
-else
-	echo asdf is here
-fi
--- wip/start_over.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cd ~
-rm -r ~/Test
-rm -r ~/Google\ Drive/Data/Test
-mkdir ~/Test
-mkdir ~/Google\ Drive/Data/Test
-touch ~/Test/local.file
-touch ~/Google\ Drive/Data/Test/GDrive.file
